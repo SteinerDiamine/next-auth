ca3f1b596d4d643b564c8ff2c510b23ce1ccf6d7
SteinerDiamine <shubhamsharma68663@gmail.com>
2025-04-19 09:17:56 +0530

    commit

--- components/ui/inputone.tsx
+++ components/ui/inputone.tsx
@@ -62,9 +62,10 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 import { useMotionTemplate, useMotionValue, motion } from "motion/react";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  // TODO: Add custom props here in future
-}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
