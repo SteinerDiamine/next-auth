a18231dfe12029ff1f6b741fb58e5f533fd9d4bc
SteinerDiamine <shubhamsharma68663@gmail.com>
2025-05-02 19:02:06 +0530

    commit

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,44 +1,76 @@
+import React from "react";
+
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import ServerSidebar from "@/components/server/server-sidebar";
+
+
+export default async function ServerIdLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) {
+  const profile = await currentProfile();
+
+  if (!profile) return redirect("/");
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  });
+
+  if (!server) return redirect("/");
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
+}
 
 
 
-
-// import { ReactNode } from "react";
+// import ServerSidebar from "@/components/server/server-sidebar";
+// import { currentProfile } from "@/lib/current-profile";
+// import { db } from "@/lib/db";
 
 // import { redirect } from "next/navigation";
-// import { db } from "@/lib/db";
-// import { currentProfile } from "@/lib/current-profile";
-// import ServerSidebar from "@/components/server/server-sidebar";
-
-
 
 // interface ServerIdLayoutProps {
-//   children: ReactNode;
-//   params: { serverId: string };
+//   params: Promise<{ serverId: string }>;
+//   children: React.ReactNode;
 // }
 
-// const ServerIdLayout = async ({
-//   children,
-//   params,
-// }: ServerIdLayoutProps) => {
+// const ServerLayout: React.FC<ServerIdLayoutProps> = async ({ children, params }) => {
+//   const { serverId } = await params;
 //   const profile = await currentProfile();
 
 //   if (!profile) {
-//     return redirect("/auth/login");
+//     redirect("/");
 //   }
 
 //   const server = await db.server.findUnique({
 //     where: {
 //       id: params.serverId,
-//       members: {
-//         some: {
-//           profileId: profile.id,
-//         },
-//       },
+//       members: { some: { profileId: profile.id } },
 //     },
 //   });
 
 //   if (!server) {
-//     return redirect("/");
+//     redirect("/");
 //   }
 
 //   return (
@@ -52,53 +84,3 @@
 // };
 
 // export default ServerIdLayout;
-
-
-
-
-
-
-
-
-
-
-import ServerSidebar from "@/components/server/server-sidebar";
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
-
-import { redirect } from "next/navigation";
-
-interface ServerIdLayoutProps {
-  children: React.ReactNode;
-  params: { serverId: string };
-}
-
-const ServerIdLayout = async ({ children, params }: ServerIdLayoutProps) => {
-  const profile = await currentProfile();
-
-  if (!profile) {
-    redirect("/");
-  }
-
-  const server = await db.server.findUnique({
-    where: {
-      id: params.serverId,
-      members: { some: { profileId: profile.id } },
-    },
-  });
-
-  if (!server) {
-    redirect("/");
-  }
-
-  return (
-    <div className="h-full">
-      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        <ServerSidebar serverId={params.serverId} />
-      </div>
-      <main className="h-full md:pl-60">{children}</main>
-    </div>
-  );
-};
-
-export default ServerIdLayout;
